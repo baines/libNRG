3cb5d7c29f6103494af9b09869ac0a9e586c6568
Alex Baines <A.Baines@warwick.ac.uk>
2012-12-11 18:20:26 +0000

    put EntityHelper in the nrg namespace

--- examples/test3.cpp
+++ examples/test3.cpp
@@ -1,7 +1,7 @@
 #include "nrg.h"
 #include <iostream>
 
-struct TestEntity : EntityHelper<TestEntity, 1337> {
+struct TestEntity : nrg::EntityHelper<TestEntity, 1337> {
 	TestEntity() : f(this){};
 	virtual void getFields(nrg::FieldList& list) {
 		list.add(f);
--- include/nrg_util.h
+++ include/nrg_util.h
@@ -1,11 +1,15 @@
 #ifndef NRG_UTIL_H
 #define NRG_UTIL_H
 
+namespace nrg {
+
 template<class T, uint16_t type>
 struct EntityHelper : nrg::Entity {
 	virtual nrg::Entity* clone(){ return new T(*static_cast<T*>(this)); }
 	virtual uint16_t getType() const { return type; }
 };
+
+}
 
 #endif
 
